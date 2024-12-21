17477a1a6d00fbb7a2568ec3ae61e3834c3b72e3
Bradley Remedios <bremedios@gmail.com>
2024-12-21 10:18:48 -0800

    The image no longer throws away the buildroot ccache but saves it.

--- interactive.sh
+++ interactive.sh
@@ -35,6 +35,6 @@ if [ -z $TARGET ] ; then
     exit 1
 fi
 
-echo docker run -it --mount type=volume,src=knulli_build-$TARGET,target=/home/developer/build --mount type=volume,src=knulli_toolchain-$TARGET,target=/home/developer/toolchains --mount type=bind,src=./output,target=/home/developer/output bremedios/knulli-build-interactive:latest
+echo docker run -it --mount type=volume,src=knulli_build-$TARGET,target=/home/developer/build --mount type=volume,src=knulli_toolchain-$TARGET,target=/home/developer/toolchains --mount type=volume,src=knulli_buildroot_cache-$TARGET,target=/home/developer/.buildroot-ccache --mount type=bind,src=./output,target=/home/developer/output bremedios/knulli-build-interactive:latest
 
-docker run --rm -it --mount type=volume,src=knulli_build-$TARGET,target=/home/developer/build --mount type=volume,src=knulli_toolchain-$TARGET,target=/home/developer/toolchains --mount type=bind,src=./output,target=/home/developer/output bremedios/knulli-build-interactive:latest
+docker run --rm -it --mount type=volume,src=knulli_build-$TARGET,target=/home/developer/build --mount type=volume,src=knulli_toolchain-$TARGET,target=/home/developer/toolchains --mount type=volume,src=knulli_buildroot_cache-$TARGET,target=/home/developer/.buildroot-ccache --mount type=bind,src=./output,target=/home/developer/output bremedios/knulli-build-interactive:latest
